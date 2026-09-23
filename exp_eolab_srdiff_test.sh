bd06118843cd9c24f6ca8fb0539c0a91b59c9c31
Hubert Kanyamahanga <kanyamahanga.hubert@aims-senegal.org>
2026-09-23 14:46:57 +0200

    Updated

--- exp_eolab_srdiff_test.sh
+++ exp_eolab_srdiff_test.sh
@@ -22,10 +22,21 @@
 # python trainer.py --config_file=./configs/train_main/ --exp_name srdiff_maxvit_ltae_ckpt --hparams="diff_net_ckpt=/hubert_storage/Results/LCC_FLAIR_HUB_SRDiff_CLIP_DDPM_V_SR_DPM/checkpoints/srdiff_maxvit_ltae_ckpt" --infer
  
 
-export CONDA_ENVS_PATH=$HOME/.conda/envs
+# export CONDA_ENVS_PATH=$HOME/.conda/envs
+# DATA_DIR="/hubert_storage/"
+# export DATA_DIR
+# source /home/eouser/flair_venv/bin/activate
+# which python
+# cd $HOME/exp_2026/LCC_FLAIR_HUB_SRDiff_CLIP_DDPM_V_SR_DPM
+# python trainer.py --config_file=./configs/train_main/ --exp_name srdiff_maxvit_ltae_ckpt --reset
+
+
+export CONDA_ENVS_PATH=$HOME/miniconda3/envs
 DATA_DIR="/hubert_storage/"
 export DATA_DIR
-source /home/eouser/flair_venv/bin/activate
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate /hubert_storage/flair_venv
 which python
-cd $HOME/exp_2026/LCC_FLAIR_HUB_SRDiff_CLIP_DDPM_V_SR_DPM
-python trainer.py --config_file=./configs/train_main/ --exp_name srdiff_maxvit_ltae_ckpt --reset
+cd $HOME/exp_2026/LCC_FLAIR_HUB_SRDiff_DDPM_V_SR_DPM
+python trainer.py --config_file=./configs/train_main/ --exp_name srdiff_maxvit_ltae_ckpt --hparams="diff_net_ckpt=/hubert_storage/Results/LCC_FLAIR_HUB_SRDiff_DDPM_V_SR_DPM/checkpoints/srdiff_maxvit_ltae_ckpt" --infer
+
--- exp_eolab_srdiff_train.sh
+++ exp_eolab_srdiff_train.sh
@@ -12,20 +12,23 @@
 # #SBATCH --error logs/exp_LCC_FLAIR_HUB_SRDiff_CLIP_DDPM_V_SR_DPM_train_%j.err
 # source load_modules.sh
 
-# export CONDA_ENVS_PATH=$HOME/miniconda3/envs
+
+# export CONDA_ENVS_PATH=$HOME/.conda/envs
 # DATA_DIR="/hubert_storage/"
 # export DATA_DIR
-# source ~/miniconda3/etc/profile.d/conda.sh
-# conda activate /hubert_storage/flair_venv
+# source /home/eouser/flair_venv/bin/activate
 # which python
 # cd $HOME/exp_2026/LCC_FLAIR_HUB_SRDiff_CLIP_DDPM_V_SR_DPM
 # python trainer.py --config_file=./configs/train_main/ --exp_name srdiff_maxvit_ltae_ckpt --reset
 
 
-export CONDA_ENVS_PATH=$HOME/.conda/envs
+export CONDA_ENVS_PATH=$HOME/miniconda3/envs
 DATA_DIR="/hubert_storage/"
 export DATA_DIR
-source /home/eouser/flair_venv/bin/activate
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate /hubert_storage/flair_venv
 which python
 cd $HOME/exp_2026/LCC_FLAIR_HUB_SRDiff_CLIP_DDPM_V_SR_DPM
 python trainer.py --config_file=./configs/train_main/ --exp_name srdiff_maxvit_ltae_ckpt --reset
+
+
